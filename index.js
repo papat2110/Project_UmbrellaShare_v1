8283dc2151f3627b89cb3bc8dbf0c2a1b8c83d6a
papatsorn <papatsorndawthaisong@kkumail.com>
2021-08-17 00:55:32 +0700

    #

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+mongoose.Promise = global.Promise;
+mongoose
+  .connect("mongodb://localhost:27017/DHT22", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("connected true");
+    },
+    (error) => {
+      console.log("connected error");
+      process.exit()
+    }
+  );
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`Example app listening at http://localhost:8080`)
-})
+  console.log(`Example app listening at http://localhost:8080`);
+});
