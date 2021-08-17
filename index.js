0709b3ae9520b9af5e4368f0e7a5f00441031519
papatsorn <papatsorndawthaisong@kkumail.com>
2021-08-17 20:59:14 +0700

    #

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 var cors = require("cors");
 const mongoose = require("mongoose");
 
+const User = require("./models/User");
+
 mongoose.Promise = global.Promise;
 mongoose
-  .connect("mongodb://localhost:27017/DHT22", {
+  .connect("mongodb://localhost:27017/umbrellaserver", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -15,14 +17,27 @@ mongoose
     },
     (error) => {
       console.log("connected error");
-      process.exit()
+      process.exit();
     }
   );
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/user", async (req, res) => {
+  let user = await User.find();
+  console.log(user);
+  res.send(user);
+});
+
+app.post("/user", async (req, res) => {
+  let user = await new User({name:"aaa"}).save()
+  console.log(user);
+  res.send(user);
 });
 
 app.listen(process.env.PORT || 8080, () => {
