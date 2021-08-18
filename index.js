0c9ad8b3cdcbce9c956727c60bec5992ea2b6689
papatsorn <papatsorndawthaisong@kkumail.com>
2021-08-18 21:53:40 +0700

    #

--- index.js
+++ index.js
@@ -65,11 +65,8 @@ app.get('/writestt/:id/:stt', async (req, res) => {
   let userid = req.params.id;
   let status = req.params.stt;
   let userstatus = await new Status({userid:userid,status:status}).save()
-  console.log(userstatus);
-  res.send(userstatus);
-  let user = await User.find();
-  console.log(user);
-  res.send(user);
+  // console.log(userstatus);
+  // res.send(userstatus);
 });
 
 
