362a80895fe8b4b311c17f77d8517d22d1d7991a
papatsorn <papatsorndawthaisong@kkumail.com>
2021-08-22 22:49:14 +0700

    #

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get('/writestt/:id/:stt', async (req, res) => {
   let user = await User.findOne({p_id:userid});
   // res.send(user);
   if(user){
-    res.send("correct password");
+    res.send("correct id");
     let userstatus = await new Status({userid:userid,status:status}).save()
     console.log(userstatus);
     res.send(userstatus);
