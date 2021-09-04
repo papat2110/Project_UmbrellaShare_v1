3e41a0e65d3f5a3b3f381ce50fdc30404bf2a596
papatsorn <papatsorndawthaisong@kkumail.com>
2021-09-05 01:01:01 +0700

    #

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ app.get('/get_umbrella/:node_ip', async (req, res) => {
   let node_ip= req.params.node_ip;
   let umbrella = await Realtime.findOne({node_ip:node_ip}).sort({ _id: -1 }).limit(10);
   res.send("@node : "+node_ip+"\n#umbrella_id : "+umbrella.umbrella_id);
-  await Status.findByIdAndDelete(umbrella._id);
+  await Realtime.findByIdAndDelete(umbrella._id);
 });
 
 
