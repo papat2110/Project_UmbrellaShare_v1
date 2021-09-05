518c2694aa6b833e02217a2ef0ca0d1d6c26db61
papatsorn <papatsorndawthaisong@kkumail.com>
2021-09-05 21:21:28 +0700

    #

--- index.js
+++ index.js
@@ -317,9 +317,9 @@ app.get("/addlocker/:node_ip/:locker/:degree/:locker_status", async (req, res) =
 app.get("/getlocker/:node_ip", async (req, res) => {
   var node_ip = req.params.node_ip;
   var getlocker = await Locker.find({node_ip:node_ip});
-  var locker = req.params.getlocker.locker;
-  var degree = req.params.getlocker.degree;
-  var locker_status = req.params.getlocker.locker_status;
+  var locker = getlocker.locker;
+  var degree = getlocker.degree;
+  var locker_status = getlocker.locker_status;
   var show = locker+":"+degree+":"+locker_status;
   console.log(show);
   // res.send(getlocker.locker);
