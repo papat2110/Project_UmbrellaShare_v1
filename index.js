190878e3e722f4e7128e86f55cc232cda7478939
papatsorn <papatsorndawthaisong@kkumail.com>
2021-10-17 23:35:29 +0700

    #

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '15MB' }));
 
 var port = process.env.PORT || 80;
 
