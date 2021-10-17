34f3c2f032e6fb6e06c72493740f1602d790dee0
papatsorn <papatsorndawthaisong@kkumail.com>
2021-10-18 00:08:27 +0700

    #

--- index.js
+++ index.js
@@ -446,15 +446,9 @@ app.post("/picture/:user_id/:borrow_id/:status", async (req, res) => {
   var picture = req.body.imgsource;
   var name = Date.now()+".png";
   fs.writeFile('./picture/'+name, req.body.imgsource, 'base64', (err) => {
-		if (err){
-      console.log("error");
-      res.send("error");
-    }
-    else{
-      console.log("picture");
-      res.send("picture");
-    }
+    if (err) throw err
 	})
+  res.status(200)
   // if(status=="bb"){
   //   var addpicture = await new Picture({user_id:user_id,borrow_id:borrow_id,borrow_pic:name}).save()
   //   console.log(addpicture);
