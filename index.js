d260309fb70b88b1d51831da17118a7e2bd243ff
papatsorn <papatsorndawthaisong@kkumail.com>
2021-10-18 23:17:28 +0700

    #

--- index.js
+++ index.js
@@ -441,26 +441,26 @@ app.get("/getlocker/:node_ip", async (req, res) => {
 
 //add picture
 app.post("/picture/:user_id/:borrow_id/:status", async (req, res) => {
-  // var user_id = req.params.user_id;
-  // var borrow_id = req.params.borrow_id;
-  // var status = req.params.status;
-  // var picture = req.body.imgsource;
-  // var name = Date.now()+".png";
+  var user_id = req.params.user_id;
+  var borrow_id = req.params.borrow_id;
+  var status = req.params.status;
+  var picture = req.body.imgsource;
+  var name = Date.now()+".png";
   // var a = req.body.a;
   // res.send(a);
-  fs.writeFile('./picture/omg.jpg', req.body.imgsource, 'base64', function(err) {
+  fs.writeFile('./picture/'+name, req.body.imgsource, 'base64', function(err) {
     res.send(req.body.imgsource);
 	})
   res.status(200)
-  // if(status=="bb"){
-  //   var addpicture = await new Picture({user_id:user_id,borrow_id:borrow_id,borrow_pic:name}).save()
-  //   console.log(addpicture);
-  //   res.send(addpicture);
-  // }else if(status=="bg"){
-  //   var picture_update = await Picture.findOne({borrow_id:borrow_id});
-  //   var query = {_id:picture_update._id};
-  //   await Picture.findOneAndUpdate(query,{getting_pic:name});
-  //   console.log("success");
-  //   res.send("success");
-  // }
+  if(status=="bb"){
+    var addpicture = await new Picture({user_id:user_id,borrow_id:borrow_id,borrow_pic:name}).save()
+    console.log(addpicture);
+    res.send(addpicture);
+  }else if(status=="bg"){
+    var picture_update = await Picture.findOne({borrow_id:borrow_id});
+    var query = {_id:picture_update._id};
+    await Picture.findOneAndUpdate(query,{getting_pic:name});
+    console.log("success");
+    res.send("success");
+  }
 });
