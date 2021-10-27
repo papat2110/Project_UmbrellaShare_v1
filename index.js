c27c3477aaf28a40ddfc600124e5f420da60bac8
papatsorn <papatsorndawthaisong@kkumail.com>
2021-10-27 22:20:12 +0700

    #

--- index.js
+++ index.js
@@ -541,8 +541,8 @@ app.get("/timeover/:id", async (req, res) => {
     var valid = Date.now() - 1000;
     // console.log(valid);
     // res.send(valid);
-    console.log(Number(borrow_time) - 1000);
-    res.send(Number(borrow_time) - 1000);
+    console.log(1000000 - 1000);
+    res.send(1000000 - 1000);
   }
   // var borrow_time = Number(borrow.borrow_time);
   // var getting_time = Date.now();
