a1fb8afcaad8083362aaba30c1e6958276cca180
papatsorn <papatsorndawthaisong@kkumail.com>
2021-11-06 00:18:44 +0700

    #

--- index.js
+++ index.js
@@ -538,9 +538,9 @@ app.get("/timeover/:id", async (req, res) => {
     var a = valid.toString();
     if(a>0.001){
       var user = await User.findOne({p_id:usr});
-      var email = user.email;
-      console.log(user);
-      res.send(user);
+      var mail = user.email;
+      console.log(mail);
+      res.send(mail);
       // let info = await transporter.sendMail({
       //   from: '"Umbrella Share KKU" <umbrellasharekku@gmail.com>', // อีเมลผู้ส่ง
       //   to: email, // อีเมลผู้รับ สามารถกำหนดได้มากกว่า 1 อีเมล โดยขั้นด้วย ,(Comma)
