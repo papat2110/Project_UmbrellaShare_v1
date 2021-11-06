8dbdd2afccc02078df765018db597502c74c7918
papatsorn <papatsorndawthaisong@kkumail.com>
2021-11-06 23:22:01 +0700

    #

--- index.js
+++ index.js
@@ -294,13 +294,15 @@ app.get('/borrow/:user_id/:umbrella_id/:borrow_time/:borrow_place/:getting_time/
   // let borrow_time = req.params.borrow_time;
   let borrow_time = Date.now();
   let borrow_place = req.params.borrow_place;
+  let noti = await Place.findOne({node_ip:borrow_place});
+  let place_name = noti.place;
   let getting_time = req.params.getting_time;
   let getting_place = req.params.getting_place;
   let time = getting_time - borrow_time;
   let status = req.params.status;
 
   let borrow = await new Borrow({user_id:user_id,umbrella_id:umbrella_id,borrow_time:borrow_time,
-    borrow_place:borrow_place,getting_time:getting_time,getting_place:getting_place,time:time,status:status}).save()
+    borrow_place:place_name,getting_time:getting_time,getting_place:getting_place,time:time,status:status}).save()
   console.log("การยืมสำเร็จ");
   res.send("การยืมสำเร็จ");
 });
@@ -427,12 +429,14 @@ app.get('/deposit/:user_id/:locker/:deposit_time/:deposit_place/:return_time/:re
   let locker = req.params.locker;
   let deposit_time = Date.now();
   let deposit_place = req.params.deposit_place;
+  let noti = await Place.findOne({node_ip:deposit_place});
+  let place_name = noti.place;
   let return_time = req.params.return_time;
   let return_place = req.params.return_place;
   let status = req.params.status;
   let time = return_time - deposit_time;
   let deposit = await new Deposit({user_id:user_id,locker:locker,deposit_time:deposit_time,
-    deposit_place:deposit_place,return_time:return_time,return_place:return_place,time:time,status:status}).save()
+    deposit_place:place_name,return_time:return_time,return_place:return_place,time:time,status:status}).save()
   console.log(deposit);
   res.send(deposit);
 });
