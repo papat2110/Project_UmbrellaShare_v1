d738bfcb6a1cfbcd4678211c745c99c76e3e136b
papatsorn <papatsorndawthaisong@kkumail.com>
2022-02-25 00:39:47 +0700

    #

--- index.js
+++ index.js
@@ -149,23 +149,23 @@ app.get("/edit_place/:lat/:long/:place/:mac_address", async (req, res) => {
     var long = req.params.long;
     var place = req.params.place;
     var mac_address = req.params.mac_address;
-    console.log(place);
-    res.send(place);
-    // var place_change = await Place.findOne({ node_ip: mac_address });
-    // if (place_change) {
-    //   var query = { _id: place_change._id };
-    //   await Place.findOneAndUpdate(query, {
-    //     latitude: lat,
-    //     longitude: long,
-    //     place: place,
-    //     node_ip: mac_address
-    //   });
-    //   console.log("update success");
-    //   res.send("update success");
-    // } else if (!place_change) {
-    //   console.log("something wrong");
-    //   res.send("something wrong");
-    // }
+    // console.log(place);
+    // res.send(place);
+    var place_change = await Place.findOne({ node_ip: mac_address });
+    if (place_change) {
+      var query = { _id: place_change._id };
+      await Place.findOneAndUpdate(query, {
+        latitude: lat,
+        longitude: long,
+        place: place,
+        node_ip: mac_address
+      });
+      console.log("update success");
+      res.send("update success");
+    } else if (!place_change) {
+      console.log("something wrong");
+      res.send("something wrong");
+    }
 });
 
 app.get("/um_place", async (req, res) => {
