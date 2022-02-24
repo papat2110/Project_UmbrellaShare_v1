18a5c2b02127d377a99bd85e404bf056fbf3234c
papatsorn <papatsorndawthaisong@kkumail.com>
2022-02-24 23:21:15 +0700

    #

--- index.js
+++ index.js
@@ -127,44 +127,44 @@ app.get("/verify_email/:name/:email/:tel/:password/:pid", async (req, res) => {
   res.send("verify success");
 });
 
-// //add place
-// app.post("/addplace/:latitude/:longitude/:place/:node_ip", async (req, res) => {
-//   var latitude = req.params.latitude;
-//   var longitude = req.params.longitude;
-//   var place = req.params.place;
-//   var node_ip = req.params.node_ip;
-//   var addplace = await new Place({
-//     latitude: latitude,
-//     longitude: longitude,
-//     place: place,
-//     node_ip: node_ip,
-//   }).save();
-//   console.log(addplace);
-//   res.send(addplace);
-// });
+//add place
+app.post("/addplace/:latitude/:longitude/:place/:node_ip", async (req, res) => {
+  var latitude = req.params.latitude;
+  var longitude = req.params.longitude;
+  var place = req.params.place;
+  var node_ip = req.params.node_ip;
+  var addplace = await new Place({
+    latitude: latitude,
+    longitude: longitude,
+    place: place,
+    node_ip: node_ip,
+  }).save();
+  console.log(addplace);
+  res.send(addplace);
+});
 
-//edit place
-app.get("/edit_place/:lat/:long/:place/:mac_address",async (req, res) => {
-    var lat = req.params.lat;
-    var long = req.params.long;
-    var place = req.params.place;
-    var mac_address = req.params.mac_address;
-    var place = await Place.findOne({ place: place });
-    if (place) {
-      var query = { _id: place._id };
-      await Place.findOneAndUpdate(query, {
-        latitude: lat,
-        longitude: long,
-        place: place,
-        node_ip: mac_address
-      });
-      console.log("update success");
-      res.send("update success");
-    } else if (!place) {
-      res.send("something wrong");
-    }
-  }
-);
+// //edit place
+// app.get("/edit_place/:lat/:long/:place/:mac_address",async (req, res) => {
+//     var lat = req.params.lat;
+//     var long = req.params.long;
+//     var place = req.params.place;
+//     var mac_address = req.params.mac_address;
+//     var place = await Place.findOne({ place: place });
+//     if (place) {
+//       var query = { _id: place._id };
+//       await Place.findOneAndUpdate(query, {
+//         latitude: lat,
+//         longitude: long,
+//         place: place,
+//         node_ip: mac_address
+//       });
+//       console.log("update success");
+//       res.send("update success");
+//     } else if (!place) {
+//       res.send("something wrong");
+//     }
+//   }
+// );
 
 app.get("/um_place", async (req, res) => {
   let place = await Place.find();
