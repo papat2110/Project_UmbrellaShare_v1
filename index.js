be6c352fd5d7f77a8bc708c99b66292e604f3e62
papatsorn <papatsorndawthaisong@kkumail.com>
2022-02-26 02:05:30 +0700

    #

--- index.js
+++ index.js
@@ -1010,25 +1010,17 @@ app.get("/count_locker/:mac_address/:status", async (req, res) => {
     available[4] = locker.locker5;
     // console.log(available);
     // res.send(available);
-    var numOfTrue = 0;
-    for(var i=0;i<available.length;i++){
-      if(available[i] === "0"){
-        numOfTrue++;
+
+    for(var i = 0; i <= 4; i++){
+      if(available[i] == "1"){
+        count_available = count_available + 1;
+        console.log(count_available);
+        res.send(count_available);
+      }else{
+        console.log("hello");
+        res.send("hello");
       }
     }
-    console.log(numOfTrue);
-    res.send(numOfTrue);
-
-    // for(var i = 0; i <= 4; i++){
-    //   if(available[i] == "0"){
-    //     count_available = count_available + 1;
-    //     // console.log(count_available);
-    //     // res.send(count_available);
-    //   }else{
-    //     console.log("hello");
-    //     res.send("hello");
-    //   }
-    // }
   }
   // console.log(count_available);
   // res.send(count_available);
